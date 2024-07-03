95475c81e10c0a666dffd32f1acf7fa76101b74a
Akhmad Khanif Zyen <khanif.zyen@gmail.com>
2024-07-03 23:56:31 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# jurnal_pembelajaran
+
+Aplikasi Jurnal Pembelajaran. 
+
+## Fitur
+
+1. Auth
+2. Tambah Jurnal
+3. Laporan
---
+++ android/app/src/main/kotlin/com/example/jurnal_pembelajaran/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.jurnal_pembelajaran
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
